7fc25d8ab5a0ef15da063178277dc026bb2f0611
PraveenM24 <praveen050800@gmail.com>
2021-04-21 19:42:15 +0530

    Initial commit

--- models/alien4.js
+++ models/alien4.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 const dbSchema = new mongoose.Schema({
-    image: String,
+    image: Buffer,
 })
 
 module.exports = mongoose.model('imageupload', dbSchema)
