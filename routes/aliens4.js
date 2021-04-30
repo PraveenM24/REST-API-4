bd34f55d519135aab672545bede56aa72ff22a00
PraveenM24 <praveen050800@gmail.com>
2021-04-30 12:59:58 +0530

    Initial commit

--- routes/aliens4.js
+++ routes/aliens4.js
@@ -21,24 +21,25 @@ router.get('/:id', async(req, res) => {
     }
 })
 
-// router.post('/', async(req, res) => {
-//     const alien = new Alien({
-//         image: req.body.image,
-//     })
+router.post('/', async(req, res) => {
+    const alien = new Alien({
+        name: req.body.name,
+        image: req.body.image,
+    })
 
-//     try {
-//         const a1 = await alien.save()
-//         res.json(a1)
-//     } catch (err) {
-//         res.send('Error')
-//     }
-// })
+    try {
+        const a1 = await alien.save()
+        res.json(a1)
+    } catch (err) {
+        res.send('Error')
+    }
+})
 
-router.post(
-    "/",
-    dataController.uploadImg,
-    dataController.newImg
-  );
+// router.post(
+//     "/",
+//     dataController.uploadImg,
+//     dataController.newImg
+//   );
 
 router.delete('/:id', async(req,res) =>{
     try{
