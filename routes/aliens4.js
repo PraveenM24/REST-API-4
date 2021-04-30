e8e3c387e9178526e8a259b7c3208db1f2fdab10
PraveenM24 <praveen050800@gmail.com>
2021-04-30 13:18:43 +0530

    Initial commit

--- routes/aliens4.js
+++ routes/aliens4.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Alien = require('../models/alien4')
-//const dataController = require('../controllers/alien4')
+const dataController = require('../controllers/alien4')
 
 router.get('/', async(req, res) => {
     try{
